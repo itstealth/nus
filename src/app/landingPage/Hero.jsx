2466ef1f027702afea8789bd5c3581cc1818d955
DanishCodebase <ammarsaifi5376@gmail.com>
2025-11-14 11:02:39 +0530

    Add graduation years 2025 and 2026 to the Hero component's dropdown

--- src/app/landingPage/Hero.jsx
+++ src/app/landingPage/Hero.jsx
@@ -172,6 +172,8 @@ const Hero = () => {
             <option value="" disabled>
               Graduate Year
             </option>
+            <option value="2026">2026</option>
+            <option value="2025">2025</option>
             <option value="2024">2024</option>
             <option value="2023">2023</option>
             <option value="2022">2022</option>
